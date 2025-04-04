49966d2137d9c479af4f92ef040a9a4f4dab21e1
mimsilly <emilien.silly@epfl.ch>
2025-04-04 13:24:34 +0200

    Big training refactor

--- trainModels.py
+++ trainModels.py
@@ -2,136 +2,46 @@ from models import *
 from andi_datasets.models_phenom import models_phenom
 from helpersGeneration import *
 from helpersPlot import *
-from models import get_transformer_models
+from trainSettings import *
 import datetime
-print(datetime.datetime.now())
-
-# need to divide trajectories because they are given in pixels/s but we want trajectories in ms domain
-traj_div_factor = 100
-
-def load_validation_data():
-
-    trajs1 = np.load("./valTrajs/val1.npy") /traj_div_factor
-    trajs3 = np.load("./valTrajs/val3.npy") /traj_div_factor
-    trajs5 = np.load("./valTrajs/val5.npy") /traj_div_factor
-    trajs7 = np.load("./valTrajs/val7.npy") /traj_div_factor
-
-
-    vid1 = trajectories_to_video(trajs1,nPosPerFrame,center=True,image_props=image_props)
-    vid1,_ = normalize_images(vid1,background_mean,background_sigma,part_mean+background_mean)
-
-    vid3 = trajectories_to_video(trajs3,nPosPerFrame,center=True,image_props=image_props)
-    vid3,_ = normalize_images(vid3,background_mean,background_sigma,part_mean+background_mean)
-
-    vid5 = trajectories_to_video(trajs5,nPosPerFrame,center=True,image_props=image_props)
-    vid5,_ = normalize_images(vid5,background_mean,background_sigma,part_mean+background_mean)
-
-    vid7 = trajectories_to_video(trajs7,nPosPerFrame,center=True,image_props=image_props)
-    vid7,_ = normalize_images(vid7,background_mean,background_sigma,part_mean+background_mean)
-
-    return torch.Tensor(vid1),torch.Tensor(vid3), torch.Tensor(vid5), torch.Tensor(vid7)
-
-
-single_prediction = False
-use_regression_token = False
-use_pos_encoding = True
-tr_activation_fct = 'gelu'
-# Get all transformer models, _s stands for small, _b for big models
-models = get_transformer_models(patch_size, embed_dim, num_heads, hidden_dim, num_layers, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_s', use_regression_token= use_regression_token, single_prediction=single_prediction)
-
-models_very_small = get_transformer_models(patch_size, embed_dim//2, num_heads//2, hidden_dim//2, num_layers//2, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_vs', use_regression_token= use_regression_token, single_prediction=single_prediction)
-models.update(models_very_small)
-"""
-cnn_big =  GeneralTransformer(
-    embedding_cls=CNNEmbedding,
-    embed_kwargs=embed_kwargs,
-    embed_dim=embed_dim,
-    num_heads=num_heads,
-    hidden_dim=hidden_dim,
-    num_layers=num_layers,
-    mlp_head=twoLayerMLP,
-    dropout=dropout,
-    use_pos_encoding=use_pos_encoding,
-    tr_activation_fct=tr_activation_fct,
-    use_regression_token=use_regression_token,
-    single_prediction=single_prediction
-)
-
-deep_cnn_big = GeneralTransformer(
-            embedding_cls=DeepResNetEmbedding,
-            embed_kwargs=embed_kwargs,
-            embed_dim=embed_dim,
-            num_heads=num_heads,
-            hidden_dim=hidden_dim,
-            num_layers=num_layers,
-            mlp_head=twoLayerMLP,
-            dropout=dropout,
-            use_pos_encoding=use_pos_encoding,
-            tr_activation_fct=tr_activation_fct,
-            use_regression_token=use_regression_token,
-            single_prediction=single_prediction
-        )
-models.update({"cnn_b": cnn_big})
-models.update({"deep_cnn_b": deep_cnn_big})
-
-"""
-resnet = MultiImageLightResNet(patch_size, single_prediction=single_prediction)
-
-
-
-models.update({"resnet": resnet})
-
-# Create 1 optimizer and scheuler for each model
-optimizers = {name: optim.AdamW(model.parameters(), lr=1e-4) for name, model in models.items()}
-schedulers = {name: optim.lr_scheduler.StepLR(opt, step_size=5, gamma=0.9) for name, opt in optimizers.items()}
-print(models.keys())
-loss_function = nn.MSELoss()
-
-
-
-# Image generation parameters
-nPosPerFrame = 10 
-background_mean, background_sigma = 200,10
-part_mean, part_sigma = 500,20
-image_props={"upsampling_factor":5,
-      "background_intensity": [background_mean,background_sigma],
-      "particle_intensity": [part_mean,part_sigma],
-      "resolution": 130e-9,
-      "trajectory_unit" : 1000,
-      "output_size": 7,
-      "poisson_noise" : 1}
 
 
 
 
-verbose = False
-
-num_cycles = 100  # Number of dataset refreshes
-batch_size = 16
-
+### Models Settings ###
+# Most of models settings are in Train_settings for unity
+# Type of training: mixTrajectories for learning trajectories with a switch in the middle
 mix_trajectories = True and not single_prediction
+models, optimizers, schedulers = getTrainingModels()
 
-# number of time steps per trajectory (frames), will be divided by nPosPerFrame
-nFrames = 30
-T = nFrames * nPosPerFrame
-# number of trajectories
-N = 64
 
+### Training Settings ###
+num_cycles = 100  # Number of dataset refreshes
+batch_size = 16 # Number of sequences in 1 batch
+N = 64 # Number of sequences in per value of D in Trainings_Ds
 # Mean and variance of the trajectories of Ds
 TrainingDs_list = [[1, 1], [3, 1], [5, 1], [7, 1]]
+verbose = False
+
 
 
 # Load validation dataset (fixed, does not change across cycles)
-val_videos = load_validation_data()  # Returns (vid1, vid3, vid5, vid7)
+val_videos = load_validation_data(nFrames)  # Returns (vid1, vid3, vid5, vid7)
 val_labels = torch.tensor([1, 3, 5, 7], dtype=torch.float32)  # Corresponding labels
+
 
 # Dictionary to store validation losses per model and dataset type
 validation_losses = {name: {f"val_{label.item()}": [] for label in val_labels} for name in models.keys()}
 for models_name in validation_losses.keys():
     validation_losses[models_name].update({"val_avg":[]})
-
-# create all_labels to be saved later
+# create all_labels to be plotted later
 all_gen_labels = np.array([])  # Empty array to start
+
+
+print("Starting Training with parameters:")
+print("### Model parameters ###")
+
+print(datetime.datetime.now())
 
 
 for cycle in range(num_cycles):
@@ -179,6 +89,7 @@ for cycle in range(num_cycles):
     all_labels = np.concatenate(all_labels, axis=0)
 
 
+    # Mix Trajectories
     if mix_trajectories:
         num_sequences_per_label = all_videos.shape[0] // len(val_labels)  # Number of sequences per label
         quarter_sequences = num_sequences_per_label // 4  # N/4 sequences per label
@@ -186,6 +97,8 @@ for cycle in range(num_cycles):
         # Define the mixing pairs (modularly specify which labels to mix)
         start_index = 0
 
+        # Labels to mix between each other
+        # Currently for each label we have 2* 1/4 of sequecnes mixed and 1/2 normal 
         mixing_pairs = [
             (1, 7, start_index),  # Mix label 1 with label 7
             (1, 5, start_index + quarter_sequences),  # Mix label 1 with label 5
@@ -216,36 +129,6 @@ for cycle in range(num_cycles):
                 if(verbose):
                     print(f"Mixing {idx_a} (label {label_a}) with {idx_b} (label {label_b}) at split index {split_index}")
 
-    """
-    MIXES TRAJECTORIES TOO MUCH
-
-    if mix_trajectories:
-        num_to_mix = int(0.3 * all_videos.shape[0])  # 30% of the data
-        indices = np.arange(all_videos.shape[0])
-        np.random.shuffle(indices)
-        mix_indices = indices[:num_to_mix]
-
-        for idx in mix_indices:
-            # Randomly select another trajectory to mix with
-            other_idx = np.random.choice(indices)
-            while other_idx == idx:
-                other_idx = np.random.choice(indices)
-
-            # Randomly select the split index
-            split_index = np.random.randint(nFrames//2 - 5, nFrames//2 + 5)
-
-            # Mix the videos and labels using a temporary buffer
-            temp_videos = all_videos[idx, split_index:].copy()
-            all_videos[idx, split_index:] = all_videos[other_idx, split_index:]
-            all_videos[other_idx, split_index:] = temp_videos
-
-            temp_labels = all_labels[idx, split_index:].copy()
-            all_labels[idx, split_index:] = all_labels[other_idx, split_index:]
-            all_labels[other_idx, split_index:] = temp_labels
-            print(f"Mixing {idx} with {other_idx} at split index {split_index}")
-            print(f"Labels: {all_labels[idx,:]} <-> {all_labels[other_idx,:]}")
-    """
-
 
     # Convert to tensors
     all_videos = torch.Tensor(all_videos)
@@ -255,6 +138,8 @@ for cycle in range(num_cycles):
     dataset = ImageDataset(all_videos, all_labels)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+
+    # Training Loop
     for name, model in models.items():
         model.train()
         optimizer = optimizers[name]
@@ -279,6 +164,7 @@ for cycle in range(num_cycles):
                     #print(model.reg_token)
 
 
+    # Evaluation part
     for name, model in models.items():
         model.eval()
         with torch.no_grad():
@@ -307,7 +193,7 @@ for cycle in range(num_cycles):
 
 
 
-print(all_gen_labels.shape)
+print(f"Number of generated sequences: {all_gen_labels.shape}")
 # --- Save everything for later analysis ---
 save_path = "training_results.pth"
 results = {
---
+++ trainSettings.py
@@ -0,0 +1,237 @@
+from models import *
+from helpersGeneration import *
+
+
+# Define settings needed in the other files
+
+# Models settings
+loss_function = nn.MSELoss()
+single_prediction = False
+use_regression_token = False
+use_pos_encoding = True
+tr_activation_fct = 'gelu'
+
+# Define model hyperparameters
+patch_size = 7
+embed_dim = 64
+num_heads = 4
+hidden_dim = 128
+num_layers = 6
+dropout = 0.0
+
+
+# Image generation parameters
+traj_div_factor = 100 # need to divide trajectories because they are given in pixels/s but we want trajectories in ms domain
+
+nPosPerFrame = 10 
+nFrames = 30 # = Seuence length
+T = nFrames * nPosPerFrame
+# number of trajectories
+background_mean, background_sigma = 200,10
+part_mean, part_sigma = 500,20
+image_props={"upsampling_factor":5,
+      "background_intensity": [background_mean,background_sigma],
+      "particle_intensity": [part_mean,part_sigma],
+      "resolution": 130e-9,
+      "trajectory_unit" : 1000,
+      "output_size": 7,
+      "poisson_noise" : 1}
+
+
+
+# Change this function to 
+def getTrainingModels():
+
+    # Get all transformer models, _s stands for small, _b for big models
+    models = get_transformer_models(patch_size, embed_dim, num_heads, hidden_dim, num_layers, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_s', use_regression_token= use_regression_token, single_prediction=single_prediction)
+
+    models_very_small = get_transformer_models(patch_size, embed_dim//2, num_heads//2, hidden_dim//2, num_layers//2, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_vs', use_regression_token= use_regression_token, single_prediction=single_prediction)
+    models.update(models_very_small)
+    """
+    cnn_big =  GeneralTransformer(
+        embedding_cls=CNNEmbedding,
+        embed_kwargs=embed_kwargs,
+        embed_dim=embed_dim,
+        num_heads=num_heads,
+        hidden_dim=hidden_dim,
+        num_layers=num_layers,
+        mlp_head=twoLayerMLP,
+        dropout=dropout,
+        use_pos_encoding=use_pos_encoding,
+        tr_activation_fct=tr_activation_fct,
+        use_regression_token=use_regression_token,
+        single_prediction=single_prediction
+    )
+
+    deep_cnn_big = GeneralTransformer(
+                embedding_cls=DeepResNetEmbedding,
+                embed_kwargs=embed_kwargs,
+                embed_dim=embed_dim,
+                num_heads=num_heads,
+                hidden_dim=hidden_dim,
+                num_layers=num_layers,
+                mlp_head=twoLayerMLP,
+                dropout=dropout,
+                use_pos_encoding=use_pos_encoding,
+                tr_activation_fct=tr_activation_fct,
+                use_regression_token=use_regression_token,
+                single_prediction=single_prediction
+            )
+    models.update({"cnn_b": cnn_big})
+    models.update({"deep_cnn_b": deep_cnn_big})
+
+    """
+    resnet = MultiImageLightResNet(patch_size, single_prediction=single_prediction)
+    models.update({"resnet": resnet})
+
+    # Create 1 optimizer and scheuler for each model
+    optimizers = {name: optim.AdamW(model.parameters(), lr=1e-4) for name, model in models.items()}
+    schedulers = {name: optim.lr_scheduler.StepLR(opt, step_size=5, gamma=0.9) for name, opt in optimizers.items()}
+
+    return models, optimizers, schedulers
+
+
+
+def load_validation_data(length = 20):
+
+    length_values = [20,30]
+    if( length not in length_values):
+        ValueError(f"Invalid length value, select one in: {length_values}")
+
+    trajs1 = np.load("./valTrajs"+str(length)+"/val1.npy") /traj_div_factor
+    trajs3 = np.load("./valTrajs"+str(length)+"/val3.npy") /traj_div_factor
+    trajs5 = np.load("./valTrajs"+str(length)+"/val5.npy") /traj_div_factor
+    trajs7 = np.load("./valTrajs"+str(length)+"/val7.npy") /traj_div_factor
+
+
+    vid1 = trajectories_to_video(trajs1,nPosPerFrame,center=True,image_props=image_props)
+    vid1,_ = normalize_images(vid1,background_mean,background_sigma,part_mean+background_mean)
+
+    vid3 = trajectories_to_video(trajs3,nPosPerFrame,center=True,image_props=image_props)
+    vid3,_ = normalize_images(vid3,background_mean,background_sigma,part_mean+background_mean)
+
+    vid5 = trajectories_to_video(trajs5,nPosPerFrame,center=True,image_props=image_props)
+    vid5,_ = normalize_images(vid5,background_mean,background_sigma,part_mean+background_mean)
+
+    vid7 = trajectories_to_video(trajs7,nPosPerFrame,center=True,image_props=image_props)
+    vid7,_ = normalize_images(vid7,background_mean,background_sigma,part_mean+background_mean)
+
+    return torch.Tensor(vid1),torch.Tensor(vid3), torch.Tensor(vid5), torch.Tensor(vid7)
+
+
+
+
+
+
+
+
+
+# Define model instances
+
+def get_transformer_models(patch_size = patch_size, embed_dim = embed_dim, num_heads = num_heads, hidden_dim = hidden_dim, num_layers = num_layers,
+                            dropout= dropout, use_pos_encoding = False, tr_activation_fct='gelu', use_regression_token=True , single_prediction = True, name_suffix = ''):
+    """
+    Returns different variants of the GeneralTransformer model.
+    """
+
+
+    embed_kwargs = {"patch_size": patch_size, "embed_dim": embed_dim}
+    # Define MLP heads
+    twoLayerMLP = nn.Sequential(
+        nn.Linear(embed_dim, hidden_dim),
+        nn.ReLU(),
+        nn.Linear(hidden_dim, 1)  # Output a single scalar value
+    )
+
+    oneLayerMLP = nn.Sequential(
+        nn.Linear(embed_dim, 1)  # Output a single scalar value
+    )
+    # Define model instances
+    models = {
+        "linear_2layer" + name_suffix: GeneralTransformer(
+            embedding_cls=LinearProjectionEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "linear_1layer"+ name_suffix: GeneralTransformer(
+            embedding_cls=LinearProjectionEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=oneLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "cnn_1layer"+ name_suffix: GeneralTransformer(
+            embedding_cls=CNNEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=oneLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "deepcnn_1layer"+ name_suffix: GeneralTransformer(
+            embedding_cls=DeepResNetEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=oneLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "cnn_2layer"+ name_suffix: GeneralTransformer(
+            embedding_cls=CNNEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "deepcnn_2layer"+ name_suffix: GeneralTransformer(
+            embedding_cls=DeepResNetEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            tr_activation_fct=tr_activation_fct,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        )
+    }
+    
+    return models
