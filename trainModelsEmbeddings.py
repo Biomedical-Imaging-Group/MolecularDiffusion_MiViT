37ffa59c2494ff65c3a8f2827d248ede8158bf60
mimsilly <emilien.silly@epfl.ch>
2025-06-01 17:41:32 +0200

    Train for embeddings ready

---
+++ trainModelsEmbeddings.py
@@ -0,0 +1,244 @@
+from models import *
+from andi_datasets.models_phenom import models_phenom
+from helpersGeneration import *
+from helpersPlot import *
+from trainSettingsEmbeddings import *
+import datetime
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+
+
+
+### Models Settings ###
+# Most of models settings are in Train_settings for unity
+# Type of training: mixTrajectories for learning trajectories with a switch in the middle
+mix_trajectories = True and not single_prediction
+models, optimizers, schedulers = getTrainingModels()
+
+for name in models:
+    models[name] = models[name].to(device)
+    print(name)
+    print(sum(p.numel() for p in models[name].parameters() if p.requires_grad))
+
+
+### Training Settings ###
+num_cycles = 100  # Number of dataset refreshes
+# ToDo: Try if reducing batch_size makes the model learn the transitions
+# ToDO: Try computing loss per timeStep, or add a loss term that favorises transitions see https://chatgpt.com/c/67efd8f6-52a4-8010-a0ca-09ea0b60fa3e
+batch_size = 1 if adaptive_batch_size != -1 else 16 # Number of sequences in 1 batch
+shuffle = True # if trajectories should be shuffled during training
+N = 64 # Number of sequences in per value of D in Trainings_Ds
+# Mean and variance of the trajectories of Ds
+#TrainingDs_list = [[1, 1], [3, 1], [5, 1], [7, 1]]
+TrainingDs_list = [[1, 1], [3, 1], [5, 1], [7, 1], [9,1], [10, 0.5]]
+
+printParams = True
+verbose = False
+
+
+
+
+
+# Load validation dataset (fixed, does not change across cycles)
+val_videos = load_validation_data(nFrames)  # Returns (vid1, vid3, vid5, vid7)
+val_labels = torch.tensor([1, 3, 5, 7, 9], dtype=torch.float32)  # Corresponding labels
+
+val_labels = val_labels
+
+
+# Dictionary to store validation losses per model and dataset type
+validation_losses = {name: {f"val_{label.item()}": [] for label in val_labels} for name in models.keys()}
+for models_name in validation_losses.keys():
+    validation_losses[models_name].update({"val_avg":[]})
+# create all_labels to be plotted later
+all_gen_labels = np.array([])  # Empty array to start
+
+
+
+if(printParams):
+    print("Starting Training with parameters:")
+    # Training Settings
+    print("### Training Settings ###")
+    print(f"Num Cycles: {num_cycles}")
+    print(f"Batch Size: {batch_size}")
+    print(f"Sequences per D: {N}")
+    print(f"Training D List: {TrainingDs_list}")
+    print(f"Verbose: {verbose}")
+    print(f"Mix Trajectories: {mix_trajectories}")
+    print()
+
+    # Model Settings
+    print("### Model Hyperparameters ###")
+    print(f"Loss Function: {loss_function}")
+    print(f"Single Prediction: {single_prediction}")
+    print(f"Use Regression Token: {use_regression_token}")
+    print(f"Use Positional Encoding: {use_pos_encoding}")
+    print(f"Transformer Activation Function: {tr_activation_fct}")
+    print()
+
+    # Model Architecture
+    print("### Model Architecture ###")
+    print(f"Patch Size: {patch_size}")
+    print(f"Embedding Dimension: {embed_dim}")
+    print(f"Num Heads: {num_heads}")
+    print(f"Hidden Dimension: {hidden_dim}")
+    print(f"Num Layers: {num_layers}")
+    print(f"Dropout: {dropout}")
+    print()
+
+
+    print("### Image Generation Parameters ###")
+    print(f"Trajectory Division Factor: {traj_div_factor}")
+    print(f"Positions Per Frame: {nPosPerFrame}")
+    print(f"Number of Frames: {nFrames}")
+    print(f"Total Time Steps: {T}")
+    print(f"Image Properties: {image_props}")
+    print()
+
+
+    print("StartTime: ",datetime.datetime.now())
+
+
+for cycle in range(num_cycles):
+    
+    # adaptive batch size doubles the size of batch every adaptive_batch_size cycles
+    #https://arxiv.org/abs/1712.02029
+    if(adaptive_batch_size != -1 and cycle != 0 and cycle % adaptive_batch_size == 0):
+        batch_size = batch_size * 2
+        print(f"Cycle: {cycle} new batch size: {batch_size}")
+
+    print(f"Cycle {cycle+1} out of {num_cycles}: {(cycle+1)/num_cycles * 100:.2f}%")
+    # Generate a new batch of images and labels
+    all_videos = []
+    all_labels = []
+
+    for TrainingDs in TrainingDs_list:
+        # Generate a new batch of 2000 images and labels
+        trajs, labels = models_phenom().single_state(N, 
+                                    L=0,
+                                    T=T,
+                                    Ds=TrainingDs,  # Mean and variance
+                                    alphas=1)
+        # Reshape trajectories
+        trajs = trajs.transpose(1, 0, 2)
+        labels = labels.transpose(1, 0, 2)
+
+        # For reporting stats save generated labels 
+        all_gen_labels = np.append(all_gen_labels,labels[:, 0, 1])
+
+
+
+        if single_prediction:
+            labels = labels[:, 0, 1]
+        else:
+            labels = labels[:, :, 1]
+            labels = labels.reshape(labels.shape[0], -1, nPosPerFrame).mean(axis=2)
+
+        # Store all labels
+        all_labels.append(labels)
+
+        all_labels = all_labels 
+
+
+        # Convert trajectories of D (pixels/s) to D (micro_m/ms)
+        trajs = trajs / traj_div_factor
+        videos = trajectories_to_video(trajs, nPosPerFrame, center=center, image_props=image_props)
+        videos, _ = normalize_images(videos, background_mean, background_sigma, part_mean + background_mean)
+
+        all_videos.append(videos)
+
+    # Concatenate all generated data
+    all_videos = np.concatenate(all_videos, axis=0)
+    all_labels = np.concatenate(all_labels, axis=0)
+    # Divide Labels by D_max to have values between 0 and 1 -> better optimizers
+    all_labels = all_labels / D_max_normalization
+
+
+
+
+    # Convert to tensors
+    all_videos = torch.Tensor(all_videos)
+    all_labels = torch.Tensor(all_labels).unsqueeze(-1)  # Add an extra dimension for single_prediction
+
+    # Create a dataset and shuffle
+    dataset = ImageDataset(all_videos, all_labels)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle= mix_trajectories or shuffle)
+
+
+    # Training Loop
+    for name, model in models.items():
+        model.train()
+        optimizer = optimizers[name]
+        scheduler = schedulers[name]
+
+        for batch_images, batch_labels in dataloader:
+
+            batch_images = batch_images.to(device)
+            batch_labels = batch_labels.to(device)
+
+            optimizer.zero_grad()
+            
+            predictions = model(batch_images)
+
+            loss = loss_function(predictions, batch_labels)
+            loss.backward()
+            optimizer.step()
+
+        # Step the learning rate scheduler after training
+        scheduler.step()
+        
+        if(verbose):
+            for p_name, param in model.named_parameters():
+                if param.grad is not None:
+                    print(f"{name}: {p_name}: Mean : {param.grad.abs().mean().item():.8f} Max:  {param.grad.abs().max().item():.8f}")
+                    #print(model.reg_token)
+
+
+    # Evaluation part
+    for name, model in models.items():
+        model.eval()
+        with torch.no_grad():
+            label_losses = []
+            for vid, label_value in zip(val_videos, val_labels):
+                vid = vid.to(device)
+
+
+                # Adjust label shape based on single_prediction
+                if not model.single_prediction:
+                    batch_size, num_images, _, _ = vid.shape
+                    label = torch.full((batch_size, num_images, 1), label_value, device=device)  # Shape: [batch_size, num_images, 1]
+                else:
+                    label = torch.full((vid.shape[0],), label_value, device=device).view(-1, 1)  # Shape: [batch_size, 1]
+
+
+                # Multiply Labels by D_max to have values between 0 and 10 
+                val_predictions = model(vid) * D_max_normalization
+                val_loss = loss_function(val_predictions, label)
+                avg_val_loss = val_loss.item()
+                validation_losses[name][f"val_{label_value.item()}"].append(avg_val_loss)
+                if(verbose):
+                    print(f"{name} on val_{label_value.item()}: Validation Loss = {avg_val_loss:.4f}")
+                label_losses.append(avg_val_loss)
+            # Compute average across all labels
+            avg_val_avg = np.mean(label_losses)  # Use numpy for mean calculation
+            validation_losses[name]["val_avg"].append(avg_val_avg)
+            if(verbose):
+                print(f"{name} on val_avg: Validation Loss = {avg_val_avg:.4f}")
+
+
+
+
+print(f"Number of generated sequences: {all_gen_labels.shape}")
+# --- Save everything for later analysis ---
+save_path = "training_resultsEmbed.pth"
+results = {
+    "validation_losses": validation_losses,
+    "all_labels": all_gen_labels,  # Convert to NumPy for easier histogram plotting
+    "model_weights": {name: model.state_dict() for name, model in models.items()}
+}
+torch.save(results, save_path)
+print(f"\nTraining results saved to {save_path}")
+print(datetime.datetime.now())
---
+++ trainSettingsEmbeddings.py
@@ -0,0 +1,207 @@
+from models import *
+from helpersGeneration import *
+
+
+# Define settings needed in the other files
+
+sequences = False
+center = True
+# adaptive batch size doubles the size of batch every adaptive_batch_size cycles
+# set to -1 if no adaptive batch_size 
+adaptive_batch_size = 20
+lr = 1e-4
+D_max_normalization = 10
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+if(sequences):
+    # Models settings
+    loss_function = nn.MSELoss()
+    val_loss_function = nn.MSELoss(reduction='none')
+    single_prediction = False
+    use_regression_token = False
+    use_pos_encoding = True
+    tr_activation_fct = F.relu
+else:
+    # Models settings
+    loss_function = nn.MSELoss()
+    val_loss_function = nn.MSELoss(reduction='none')
+    single_prediction = True
+    use_regression_token = True
+    use_pos_encoding = True
+    tr_activation_fct = F.relu
+
+
+# tr_activation_fct = F.LeakyReLU, F.GELU F.ReLU
+
+# Define model hyperparameters
+patch_size = 9
+embed_dim = 64
+num_heads = 4
+hidden_dim = 128
+num_layers = 6
+dropout = 0.0
+
+
+# Image generation parameters
+traj_div_factor = 100 # need to divide trajectories because they are given in pixels/s but we want trajectories in ms domain
+
+nPosPerFrame = 10 
+nFrames = 30 # = Seuence length
+T = nFrames * nPosPerFrame
+# number of trajectories
+# values from Real data
+background_mean,background_sigma = 1420, 290
+part_mean, part_std = 6000 - background_mean,500
+
+image_props = {
+    "particle_intensity": [
+        part_mean,
+        part_std,
+    ],  # Mean and standard deviation of the particle intensity
+    "NA": 1.46,  # Numerical aperture
+    "wavelength": 500e-9,  # Wavelength
+    "psf_division_factor": 1.3,  
+    "resolution": 100e-9,  # Camera resolution or effective resolution, aka pixelsize
+    "output_size": patch_size,
+    "upsampling_factor": 5,
+    "background_intensity": [
+        background_mean,
+        background_sigma,
+    ],  # Standard deviation of background intensity within a video
+    "poisson_noise": 100,
+    "trajectory_unit" : 1200
+}
+
+
+
+
+# Change this function to 
+def getTrainingModels(lr=1e-4):
+
+    # Get all transformer models, _s stands for small, _b for big models
+    models = get_transformer_models(patch_size, embed_dim, num_heads, hidden_dim, num_layers, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_n', use_regression_token= use_regression_token, single_prediction=single_prediction)
+    models_small = get_transformer_models(patch_size, embed_dim//2, num_heads//2, hidden_dim//2, num_layers//2, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_s', use_regression_token= use_regression_token, single_prediction=single_prediction)
+    models_big = get_transformer_models(patch_size, embed_dim*2, num_heads*2, hidden_dim*2, num_layers*2, dropout, use_pos_encoding=use_pos_encoding,tr_activation_fct=tr_activation_fct,name_suffix='_b', use_regression_token= use_regression_token, single_prediction=single_prediction)
+
+    models.update(models_small)
+    models.update(models_big)
+
+    resnet = MultiImageResNet(patch_size, single_prediction=single_prediction, activation=nn.ReLU)
+    models.update({"resnet": resnet})
+
+    # Create 1 optimizer and scheuler for each model
+    optimizers = {name: optim.AdamW(model.parameters(), lr=lr) for name, model in models.items()}
+    schedulers = {name: optim.lr_scheduler.StepLR(opt, step_size=5, gamma=0.9) for name, opt in optimizers.items()}
+
+    return models, optimizers, schedulers
+
+
+val_d_in_order = np.arange(0.1,10.01,0.1)
+N_in_order = 10 # number of particles
+
+def load_validation_data(length = 20):
+
+    length_values = [20,30]
+    if( length not in length_values):
+        ValueError(f"Invalid length value, select one in: {length_values}")
+
+    trajs1 = np.load("./valTrajs"+str(length)+"/val1.npy") /traj_div_factor
+    trajs3 = np.load("./valTrajs"+str(length)+"/val3.npy") /traj_div_factor
+    trajs5 = np.load("./valTrajs"+str(length)+"/val5.npy") /traj_div_factor
+    trajs7 = np.load("./valTrajs"+str(length)+"/val7.npy") /traj_div_factor
+    trajs9 = np.load("./valTrajs"+str(length)+"/val9.npy") /traj_div_factor
+
+    trajs_in_order = np.load("./valTrajsInOrderImFt.npy") /traj_div_factor
+    print(trajs_in_order.shape)
+
+    vid1 = trajectories_to_video(trajs1,nPosPerFrame,center=True,image_props=image_props)
+    vid1,_ = normalize_images(vid1,background_mean,background_sigma,part_mean+background_mean)
+
+    vid3 = trajectories_to_video(trajs3,nPosPerFrame,center=True,image_props=image_props)
+    vid3,_ = normalize_images(vid3,background_mean,background_sigma,part_mean+background_mean)
+
+    vid5 = trajectories_to_video(trajs5,nPosPerFrame,center=True,image_props=image_props)
+    vid5,_ = normalize_images(vid5,background_mean,background_sigma,part_mean+background_mean)
+
+    vid7 = trajectories_to_video(trajs7,nPosPerFrame,center=True,image_props=image_props)
+    vid7,_ = normalize_images(vid7,background_mean,background_sigma,part_mean+background_mean)
+
+    vid9 = trajectories_to_video(trajs9,nPosPerFrame,center=True,image_props=image_props)
+    vid9,_ = normalize_images(vid9,background_mean,background_sigma,part_mean+background_mean)
+
+    trajs_in_order = trajs_in_order.reshape(-1,T,2)
+    vid_in_order =  trajectories_to_video(trajs_in_order,nPosPerFrame,center=True,image_props=image_props)
+    vid_in_order = vid_in_order.reshape(len(val_d_in_order),N_in_order,nFrames,patch_size,patch_size)
+    vid_in_order,_ = normalize_images(vid_in_order,background_mean,background_sigma,part_mean+background_mean)
+
+    return torch.Tensor(vid1),torch.Tensor(vid3), torch.Tensor(vid5), torch.Tensor(vid7), torch.Tensor(vid9), torch.Tensor(vid_in_order)
+
+
+
+
+
+
+# Define model instances
+
+def get_transformer_models(patch_size = patch_size, embed_dim = embed_dim, num_heads = num_heads, hidden_dim = hidden_dim, num_layers = num_layers,
+                            dropout= dropout, use_pos_encoding = False, tr_activation_fct='gelu', use_regression_token=True , single_prediction = True, name_suffix = ''):
+    """
+    Returns different variants of the GeneralTransformer model.
+    """
+
+
+    embed_kwargs = {"patch_size": patch_size, "embed_dim": embed_dim}
+    # Define MLP heads
+    twoLayerMLP = MLPHead
+
+
+    # Define model instances
+    models = {
+        "linear" + name_suffix: GeneralTransformer(
+            embedding_cls=LinearProjectionEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            tr_activation_fct=tr_activation_fct,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "cnn"+ name_suffix: GeneralTransformer(
+            embedding_cls=CNNEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            tr_activation_fct=tr_activation_fct,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        ),
+        "deepcnn"+ name_suffix: GeneralTransformer(
+            embedding_cls=DeepResNetEmbedding,
+            embed_kwargs=embed_kwargs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            mlp_head=twoLayerMLP,
+            tr_activation_fct=tr_activation_fct,
+            dropout=dropout,
+            use_pos_encoding=use_pos_encoding,
+            use_regression_token=use_regression_token,
+            single_prediction=single_prediction
+        )
+
+    }
+    
+    return models
